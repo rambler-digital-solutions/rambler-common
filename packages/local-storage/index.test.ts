731b8c46abac428bcce5bb75f91db7b5b25f9911
Andrey Polischuk <me@andrepolischuk.com>
2023-09-21 14:30:53 +0300

    feat(local-storage): init

---
+++ packages/local-storage/index.test.ts
@@ -0,0 +1,44 @@
+import {getItem, setItem, removeItem} from './'
+
+class LocalStorage implements LocalStorage {
+  map: {[key: string]: any} = {}
+  getItem(key: string): void {
+    return this.map[key]
+  }
+  setItem(key: string, value: any): void {
+    this.map[key] = value
+  }
+  removeItem(key: string): void {
+    delete this.map[key]
+  }
+}
+
+Object.defineProperty(window, 'localStorage', {
+  value: new LocalStorage()
+})
+
+const TEST_VALUES: Record<string, any> = {
+  string: 'bar',
+  boolean: true,
+  number: 1,
+  object: {foo: true},
+  array: [1, 2, 'asd']
+}
+
+Object.entries(TEST_VALUES).forEach(([key, value]) => {
+  test(`localStorage ${key}`, () => {
+    expect(getItem(key)).toBeNull()
+    setItem(key, value)
+    expect(getItem(key)).toEqual(value)
+    removeItem(key)
+    expect(getItem(key)).toBeNull()
+  })
+
+  test(`localStorage raw ${key}`, () => {
+    expect(getItem(`raw${key}`, {raw: true})).toBeNull()
+    setItem(`raw${key}`, value, {raw: true})
+    expect(getItem(`raw${key}`, {raw: true})).toEqual(String(value))
+    removeItem(`raw${key}`)
+    expect(getItem(`raw${key}`, {raw: true})).toBeNull()
+  })
+})
---
+++ packages/local-storage/index.ts
@@ -0,0 +1,44 @@
+/** Local storage item options */
+export interface StorageItemOptions {
+  /** Use the raw value, by default the value is converted to JSON */
+  raw?: boolean
+}
+
+/** Get an item from local storage */
+export const getItem = <T>(
+  key: string,
+  options: StorageItemOptions = {}
+): T => {
+  const {raw} = options
+  let item
+
+  try {
+    const value = window.localStorage.getItem(key)
+
+    item = raw ? value : JSON.parse(value ?? '')
+  } catch {}
+
+  return item ?? null
+}
+
+/** Put an item to local storage */
+export const setItem = <T>(
+  key: string,
+  value: T,
+  options: StorageItemOptions = {}
+): void => {
+  const {raw} = options
+
+  try {
+    const resultValue = raw ? String(value) : JSON.stringify(value)
+
+    window.localStorage.setItem(key, resultValue)
+  } catch {}
+}
+
+/** Remove an item from local storage */
+export const removeItem = (key: string): void => {
+  try {
+    window.localStorage.removeItem(key)
+  } catch {}
+}
