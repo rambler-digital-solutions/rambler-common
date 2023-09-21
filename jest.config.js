c8b93891191d87afd54aba43b17b2086a994f76d
Andrey Polischuk <me@andrepolischuk.com>
2023-09-21 11:47:58 +0300

    test: fix jest testing environment

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
+/** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   moduleDirectories: ['packages', 'node_modules'],
   collectCoverage: true,
   coverageReporters: ['text'],
