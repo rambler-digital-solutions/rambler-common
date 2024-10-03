dcdf8b4efc279f9c9d04006919ad5a8d286de801
Andrey Polischuk <me@andrepolischuk.com>
2024-10-03 23:25:34 +0300

    feat(lhci-report): read report dir from lighthouse config

--- packages/lhci-report/cli.ts
+++ packages/lhci-report/cli.ts
@@ -4,18 +4,31 @@ import * as fs from 'fs/promises'
 import {formatReport} from './format'
 import {getReport, type Manifest} from './report'
 
-async function print(dir: string) {
+function readConfig(dir: string) {
+  const configFile = path.resolve(dir, 'lighthouserc')
+
+  // eslint-disable-next-line security/detect-non-literal-require
+  return require(configFile)
+}
+
+async function readManifest(dir: string) {
   const manifestFile = path.resolve(dir, 'manifest.json')
   // eslint-disable-next-line security/detect-non-literal-fs-filename
   const manifestJson = await fs.readFile(manifestFile, 'utf8')
-  const manifest: Manifest = JSON.parse(manifestJson)
+
+  return JSON.parse(manifestJson) as Manifest
+}
+
+async function print(dir: string) {
+  const manifest = await readManifest(dir)
   const report = getReport(manifest)
 
   // eslint-disable-next-line no-console
   console.log(formatReport(report))
 }
 
-const [, , reportDir] = process.argv
+const config = readConfig(process.cwd())
+const [, , reportDir = config.ci?.upload?.outputDir] = process.argv
 
 if (!reportDir) {
   throw new Error('report dir not exists')
